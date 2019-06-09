dd277bf025de35c721c4653da87ced279834afd0
apollon <Apollon76@yandex.ru>
2019-06-10 01:01:07 +0500

    Add main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 
-import numpy as np
+import cv2
+import joblib
 
 
 def main():
-    print(np.array([[1, 2, 3], [4, 5, 6]]).shape)
     try:
         image_path = sys.argv[1]
     except IndexError:
         print('Path to image should be passed')
+        return
+
+    pipeline = joblib.load('signature_detection/classifier.joblib')
+    print(pipeline.predict([cv2.imread(image_path, 0)]))
 
 
 if __name__ == '__main__':
